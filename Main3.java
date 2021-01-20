04a6ff437b763c9fa4f93253dafe6f339bbf5343
Regina Shakirova <regina.shak@yandex.ru>
2021-01-20 11:45:15 +0300

    init

---
+++ Main3.java
@@ -0,0 +1,7 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int amount = 5000;
+        int bonus_miles = amount / 20;
+        System.out.println(bonus_miles);
+    }
+}
