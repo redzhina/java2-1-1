043bf079dee732f3448634dabcc3d566665dc1fb
Regina Shakirova <regina.shak@yandex.ru>
2021-01-20 11:47:39 +0300

    initthree

--- Main3.java
+++ Main3.java
@@ -1,7 +1,7 @@
 public class Main3 {
     public static void main(String[] args) {
         int amount = 5000;
-        int bonus_miles = amount / 20;
-        System.out.println(bonus_miles);
+        int bonusMiles = amount / 20;
+        System.out.println(bonusMiles);
     }
 }
